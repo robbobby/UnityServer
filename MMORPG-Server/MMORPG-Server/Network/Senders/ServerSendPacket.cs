73deb4299f528cb3b78ffe67aeb413d4229a9759
robbobby <69080884+robbobby@users.noreply.github.com>
2021-01-19 01:09:02 +0000

    Bit of organising with regions in ServerSendPacket

--- MMORPG-Server/MMORPG-Server/Network/Senders/ServerSendPacket.cs
+++ MMORPG-Server/MMORPG-Server/Network/Senders/ServerSendPacket.cs
@@ -7,10 +7,15 @@ namespace MMORPG_Server.Network.Senders {
     /// Write the sender for that package in the ServerSendPacket class
     /// Done - call the send when you need it
     /// </summary>
+
+    #region PacketsIds enum
     public enum ServerPackets {
         Welcome = 1,
         UdpTest
     }
+    #endregion
+    // ######################################################################################################## //
+    #region Packets
     public class ServerSendPacket : ServerSend {
         public static void Welcome(int clientId, string message) {
             using Packet packet = new Packet((int) ServerPackets.Welcome);
@@ -25,4 +30,5 @@ namespace MMORPG_Server.Network.Senders {
             SendUdpData(clientId, packet);
         }
     }
+    #endregion
 }
